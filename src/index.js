56d90ee34cb9133ecbc5973acab7daa61d79ec72
Simon Hoyos <shmesa23@gmail.com>
2020-07-06 09:33:26 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+console.log('hola mundo');
+
+const obj = {
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+  key4: 'value4',
+  key5: 'value 5',
+};
+
+const arr = [1, 2, 3];
+
+arr
+  .filter((number, index) => number === number && index >= 0)
+  .map((number, index) => number * index);
